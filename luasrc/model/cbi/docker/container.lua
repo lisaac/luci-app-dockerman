f29411dea0415e23dac58baa0a689ef377a05b36
lisaac <lisaac.cn@gmail.com>
2019-11-24 21:14:35 +0800

    container:add redirect

--- luasrc/model/cbi/docker/container.lua
+++ luasrc/model/cbi/docker/container.lua
@@ -101,6 +101,7 @@ local start_stop_remove = function(m,cmd)
 end
 
 m=SimpleForm("docker", container_info.Name:sub(2), translate("Docker Contaienr") )
+m.redirect = luci.dispatcher.build_url("admin/docker/containers")
 -- m:append(Template("docker/container"))
 docker_status = m:section(SimpleSection)
 docker_status.template="docker/apply_widget"
