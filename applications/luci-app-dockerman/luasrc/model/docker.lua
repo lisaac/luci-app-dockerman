lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

luci-app-dockerman: model/docker: update options object for status_path
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -288,13 +288,15 @@ _docker.new = function()
 		debug_path = uci:get("dockerd", "globals", "debug_path") or "/tmp/.docker_debug"
 	end
 
+	local status_path = uci:get("dockerd", "globals", "status_path") or "/tmp/.docker_status"
+
 	_docker.options = {
 		host = host,
 		port = port,
 		socket_path = socket_path,
 		debug = debug,
 		debug_path = debug_path,
-		status_path = uci:get("dockerd", "globals", "status_path") or "/tmp/.docker_status"
+		status_path = status_path
 	}
 
 	local _new = docker.new(_docker.options)

lisaac/luci-app-dockerman

(cherry picked from commit https://github.com/openwrt/luci/commit/25539b2d98722adbe01927f8b563e325f54058f1 )
fix coonflicts:
	applications/luci-app-dockerman/luasrc/model/docker.lua

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -289,13 +289,15 @@ _docker.new = function()
 		debug_path = uci:get("dockerd", "dockerman", "debug_path") or "/tmp/.docker_debug"
 	end
 
+	local status_path = uci:get("dockerd", "dockerman", "status_path") or "/tmp/.docker_status"
+
 	_docker.options = {
 		host = host,
 		port = port,
 		socket_path = socket_path,
 		debug = debug,
 		debug_path = debug_path,
-		status_path = uci:get("dockerd", "dockerman", "status_path") or "/tmp/.docker_status"
+		status_path = status_path
 	}
 
 	local _new = docker.new(_docker.options)