slow-boat/luci

luci-app-dockerman: revised the information for the individual page
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

applications/luci-app-dockerman/luasrc/model/cbi/dockerman/configuration.lua
@@ -4,10 +4,10 @@
 local m, s, o
 
 m = Map("dockerd",
-	translate("Docker"),
-	translate("DockerMan is a Simple Docker manager client for LuCI"))
+	translate("Docker - Configuration"),
+	translate("DockerMan is a simple docker manager client for LuCI"))
 
-s = m:section(NamedSection, "globals", "section", translate("Setting"))
+s = m:section(NamedSection, "globals", "section", translate("Global settings"))
 
 o = s:option(Flag, "remote_endpoint",
 	translate("Remote Endpoint"),

lisaac/luci-app-dockerman

(cherry picked from commit https://github.com/openwrt/luci/commit/ad05a0187bb9b03ba5ac9df27159f54521e15e18 )
fix Conflicts:
	applications/luci-app-dockerman/luasrc/model/cbi/dockerman/configuration.lua
	applications/luci-app-dockerman/luasrc/model/cbi/dockerman/containers.lua

applications/luci-app-dockerman/luasrc/model/cbi/dockerman/configuration.lua
@@ -6,10 +6,10 @@ local uci = require "luci.model.uci"
 local m, s, o
 
 m = Map("dockerd",
-	translate("Docker"),
-	translate("DockerMan is a Simple Docker manager client for LuCI"))
+	translate("Docker - Configuration"),
+	translate("DockerMan is a simple docker manager client for LuCI"))
 
-s = m:section(NamedSection, "dockerman", "section", translate("Setting"))
+s = m:section(NamedSection, "dockerman", "section", translate("Global settings"))
 
 o = s:option(Flag, "remote_endpoint",
 	translate("Remote Endpoint"),