lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

luci-app-dockerman: refactoring remote endpoint options
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

applications/luci-app-dockerman/luasrc/model/cbi/dockerman/container.lua
@@ -705,10 +705,18 @@ elseif action == "console" then
 				luci.util.exec(kill_ttyd)
 				local hosts
 				local uci = (require "luci.model.uci").cursor()
-				local remote = uci:get("dockerd", "globals", "remote_endpoint")
-				local socket_path = (remote == "false" or not remote) and  uci:get("dockerd", "globals", "socket_path") or nil
-				local host = (remote == "true") and uci:get("dockerd", "globals", "remote_host") or nil
-				local port = (remote == "true") and uci:get("dockerd", "globals", "remote_port") or nil
+				local remote = uci:get_bool("dockerd", "globals", "remote_endpoint")
+				local host = nil
+				local port = nil
+				local socket = nil
+
+				if remote then
+					host = uci:get("dockerd", "globals", "remote_host") or nil
+					port = uci:get("dockerd", "globals", "remote_port") or nil
+				else
+					socket = uci:get("dockerd", "globals", "socket_path") or nil
+				end
+
 				if remote and host and port then
 					hosts = host .. ':'.. port
 				elseif socket_path then

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -270,13 +270,23 @@ local duplicate_config = function (self, request)
 end
 
 _docker.new = function()
+	local host = nil
+	local port = nil
+	local socket = nil
 
-	local remote = uci:get("dockerd", "globals", "remote_endpoint")
+	local remote = uci:get_bool("dockerd", "globals", "remote_endpoint")
+
+	if remote then
+		host = uci:get("dockerd", "globals", "remote_host") or nil
+		port = uci:get("dockerd", "globals", "remote_port") or nil
+	else
+		socket = uci:get("dockerd", "globals", "socket_path") or "/var/run/docker.sock"
+	end
 
 	_docker.options = {
-		host = (remote == "true") and (uci:get("dockerd", "globals", "remote_host")) or nil,
-		port = (remote == "true") and (uci:get("dockerd", "globals", "remote_port")) or nil,
-		socket_path = (remote ~= "true") and (uci:get("dockerd", "globals", "socket_path") or "/var/run/docker.sock") or nil,
+		host = host,
+		port = port,
+		socket_path = socket,
 		debug = uci:get("dockerd", "globals", "debug") == 'true' and true or false,
 		debug_path = uci:get("dockerd", "globals", "debug_path") or "/tmp/.docker_debug",
 		status_path = uci:get("dockerd", "globals", "status_path") or "/tmp/.docker_status"

lisaac/luci-app-dockerman

(cherry picked from commit https://github.com/openwrt/luci/commit/dff11ad2404d77be1da786883fa7155b83a67be0 )
fix conflicts:
	applications/luci-app-dockerman/luasrc/controller/dockerman.lua
	applications/luci-app-dockerman/luasrc/model/cbi/dockerman/container.lua
	applications/luci-app-dockerman/luasrc/model/cbi/dockerman/overview.lua
	applications/luci-app-dockerman/luasrc/model/docker.lua

applications/luci-app-dockerman/luasrc/model/cbi/dockerman/container.lua
@@ -715,10 +715,18 @@ elseif action == "console" then
 			luci.util.exec(kill_ttyd)
 			local hosts
 			local uci = (require "luci.model.uci").cursor()
-			local remote = uci:get("dockerd", "dockerman", "remote_endpoint")
-			local socket_path = (remote == "false" or not remote) and  uci:get("dockerd", "dockerman", "socket_path") or nil
-			local host = (remote == "true") and uci:get("dockerd", "dockerman", "remote_host") or nil
-			local port = (remote == "true") and uci:get("dockerd", "dockerman", "remote_port") or nil
+			local remote = uci:get_bool("dockerd", "dockerman", "remote_endpoint")
+			local host = nil
+			local port = nil
+			local socket = nil
+
+			if remote then
+				host = uci:get("dockerd", "dockerman", "remote_host") or nil
+				port = uci:get("dockerd", "dockerman", "remote_port") or nil
+			else
+				socket = uci:get("dockerd", "dockerman", "socket_path") or nil
+			end
+
 			if remote and host and port then
 				hosts = host .. ':'.. port
 			elseif socket_path then

applications/luci-app-dockerman/luasrc/model/docker.lua
@@ -270,15 +270,26 @@ local duplicate_config = function (self, request)
 end
 
 _docker.new = function()
+	local host = nil
+	local port = nil
+	local socket = nil
 
-	local remote = uci:get("dockerd", "dockerman", "remote_endpoint")
+	local remote = uci:get_bool("dockerd", "dockerman", "remote_endpoint")
+
+	if remote then
+		host = uci:get("dockerd", "dockerman", "remote_host") or nil
+		port = uci:get("dockerd", "dockerman", "remote_port") or nil
+	else
+		socket = uci:get("dockerd", "dockerman", "socket_path") or "/var/run/docker.sock" or nil
+	end
+
 	_docker.options = {
-		host = (remote == "true") and (option.host or uci:get("dockerd", "dockerman", "remote_host")) or nil,
-		port = (remote == "true") and (option.port or uci:get("dockerd", "dockerman", "remote_port")) or nil,
-		debug = option.debug or uci:get("dockerd", "dockerman", "debug") == 'true' and true or false,
-		debug_path = option.debug_path or uci:get("dockerd", "dockerman", "debug_path") or "/tmp/.docker_debug",
-		status_path = uci:get("dockerd", "dockerman", "status_path") or "/tmp/.docker_status",
-		socket_path = (remote ~= "true" or not options.host or not options.port) and (option.socket_path or uci:get("dockerd", "dockerman", "socket_path") or "/var/run/docker.sock") or nil
+		host = host,
+		port = port,
+		socket_path = socket,
+		debug = uci:get("dockerd", "dockerman", "debug") == 'true' and true or false,
+		debug_path = uci:get("dockerd", "dockerman", "debug_path") or "/tmp/.docker_debug",
+		status_path = uci:get("dockerd", "dockerman", "status_path") or "/tmp/.docker_status"
 	}
 
 	local _new = docker.new(_docker.options)